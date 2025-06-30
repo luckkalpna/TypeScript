e5cde7fe9cd585fe879c9d055bbe012018e4adf4
luckkalpna <luckkalpna@gmail.com>
2025-06-30 12:37:42 +0530

    data_type>premitivr>string

---
+++ DataTypes/Premitive/string.js
@@ -0,0 +1,7 @@
+var str = "Hello, TypoScript!";
+var str2 = 'Hello, TypoScript!';
+var str2 = "Hello, TypoScript!";
+var age = 23;
+var userName = "Kalpana";
+var info = "My name is ".concat(userName, " and I am ").concat(age, " years old.");
+console.log(info);
---
+++ DataTypes/Premitive/string.ts
@@ -0,0 +1,9 @@
+var str:string = "Hello, TypoScript!";
+var str2:string = 'Hello, TypoScript!';
+var str2:string = `Hello, TypoScript!`;
+
+var age:number = 23;
+var userName:string = "Kalpana";
+var info:string = `My name is ${userName} and I am ${age} years old.`
+
+console.log(info);
