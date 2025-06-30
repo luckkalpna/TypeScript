d6927b9107c98007da4b8226da6f91b2a842cbac
luckkalpna <luckkalpna@gmail.com>
2025-06-30 12:48:11 +0530

    data_type>premitivr>null

---
+++ DataTypes/Premitive/null.js
@@ -0,0 +1,7 @@
+var data = null;
+var val = null;
+val = "Hello, String Type!";
+console.log(typeof data);
+console.log(data);
+console.log(typeof val);
+console.log(val);
---
+++ DataTypes/Premitive/null.ts
@@ -0,0 +1,7 @@
+var data:null = null;
+var val:null | string = null;
+val = "Hello, String Type!";
+console.log(typeof data);
+console.log(data);
+console.log(typeof val);
+console.log(val);
